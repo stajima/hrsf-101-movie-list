d8f63782473eab7f872b985c270042db61b78de3
Hack Reactor Student <student@hackreactor.com>
2018-08-20 20:31:58 -0700

    Add detail panel

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,22 +1,45 @@
-const MovieItem = props => (
-  <a
-    href="#"
-    className="list-group-item list-group-item-action flex-column align-items-start"
-  >
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">{props.movie.title}</h5>
-      <button
-        type="button"
-        className={props.movie.watched ? 'btn btn-success' : 'btn btn-light'}
-        onClick={() => props.toggleWatched(props.index)}
-      >
-        Watched
-      </button>
-    </div>
-    <p className="mb-1">
-      Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget
-      risus varius blandit.
-    </p>
-    <small>Donec id elit non mi porta.</small>
-  </a>
-);
+class MovieItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({ showDetails: !this.state.showDetails });
+  }
+
+  render() {
+    return (
+      <a href="#" className="list-group-item list-group-item-action flex-column align-items-start" onClick={this.onClick}>
+        <i className={'float-right fa-lg fas fa-arrow-' + (this.state.showDetails ? 'circle-up' : 'circle-down')} />
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{this.props.movie.title}</h5>
+        </div>
+        <div className={this.state.showDetails ? '' : 'd-none'}>
+          <p className="mb-1">
+            <strong>Year:</strong> {this.props.movie.year}
+          </p>
+          <p className="mb-1">
+            <strong>Runtime:</strong> {this.props.movie.runtime}
+          </p>
+          <p className="mb-1">
+            <strong>Metascore:</strong> {this.props.movie.metascore}
+          </p>
+          <p className="mb-1">
+            <strong>IMDB Rating:</strong> {this.props.movie.rating}
+          </p>
+          <button
+            type="button"
+            className={this.props.movie.watched ? 'btn btn-success' : 'btn btn-light'}
+            onClick={() => this.props.toggleWatched(this.props.index)}
+          >
+            Watched
+          </button>
+        </div>
+      </a>
+    );
+  }
+}
--- src/data/Movies.js
+++ src/data/Movies.js
@@ -1,9 +1,44 @@
 const Movies = [
-  { title: 'Mean Girls', watched: false },
-  { title: 'Hackers', watched: false },
-  { title: 'The Grey', watched: true },
-  { title: 'Sunshine', watched: false },
-  { title: 'Ex Machina', watched: false }
+  {
+    title: 'Mean Girls',
+    watched: false,
+    rating: 0,
+    year: 1988,
+    runtime: 160,
+    metascore: 100
+  },
+  {
+    title: 'Hackers',
+    watched: false,
+    rating: 5,
+    year: 1939,
+    runtime: 453,
+    metascore: 34
+  },
+  {
+    title: 'The Grey',
+    watched: true,
+    rating: 4,
+    year: 1979,
+    runtime: 54,
+    metascore: 68
+  },
+  {
+    title: 'Sunshine',
+    watched: false,
+    rating: 5,
+    year: 1954,
+    runtime: 45,
+    metascore: 34
+  },
+  {
+    title: 'Ex Machina',
+    watched: false,
+    rating: 7,
+    year: 1930,
+    runtime: 67,
+    metascore: 40
+  }
 ];
 
 window.Movies = Movies;
